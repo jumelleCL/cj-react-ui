97683a6024a7578f52af2e8332885f0a89356f6c
Camilo <jumellecamilo@gmail.com>
2025-01-23 23:49:57 +0100

    the modal can be used now (close and open by useref)

--- src/ModalSimple/ModalSimple.tsx
+++ src/ModalSimple/ModalSimple.tsx
@@ -1,27 +1,28 @@
-import React, { ComponentProps, useRef } from "react";
+import React, { ComponentProps, forwardRef } from "react";
 import styled from "styled-components";
 import SimpleButton from "../SimpleButton/SimpleButton";
-type Props = ComponentProps<"dialog"> &{
+
+type Props = ComponentProps<"dialog"> & {
   text?: string;
   buttonText?: string;
 };
 
-const ModalSimple= ({ text, buttonText, ...rest}: Props) => {
-
-  const modalRef = useRef<HTMLDialogElement>(null);
-
-  return (
-      <Modal ref={modalRef} {...rest}>
+const ModalSimple = forwardRef<HTMLDialogElement, Props>(
+  ({ text, buttonText, ...rest }, ref) => {
+    return (
+      <Modal ref={ref} {...rest}>
         <p>{text}</p>
-        <SimpleButton onClick={() => modalRef.current?.close()}>{buttonText || 'Acept'}</SimpleButton>
+        <SimpleButton onClick={() => (ref as React.RefObject<HTMLDialogElement>).current?.close()}>
+          {buttonText || "Acept"}
+        </SimpleButton>
       </Modal>
-  );
-};
-
+    );
+  }
+);
 
 const Modal = styled.dialog`
-padding: 1rem;
-border-radius: 0.4rem;
+  padding: 1rem;
+  border-radius: 0.4rem;
 `;
 
 export default ModalSimple;
