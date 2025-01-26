cde468acc59bf28ab9c6351655e8edd1f7c26f3c
Camilo <jumellecamilo@gmail.com>
2025-01-26 02:47:46 +0100

    modal without the forwardRef

--- src/ModalSimple/ModalSimple.tsx
+++ src/ModalSimple/ModalSimple.tsx
@@ -1,24 +1,35 @@
-import React, { ComponentProps, forwardRef } from "react";
+import React, { ComponentProps, useEffect, useRef } from "react";
 import styled from "styled-components";
 import SimpleButton from "../SimpleButton/SimpleButton";
 
 type Props = ComponentProps<"dialog"> & {
   text?: string;
   buttonText?: string;
+  ref?: React.Ref<HTMLDialogElement>;
 };
 
-const ModalSimple = forwardRef<HTMLDialogElement, Props>(
-  ({ text, buttonText, ...rest }, ref) => {
-    return (
-      <Modal ref={ref} {...rest}>
-        <p>{text}</p>
-        <SimpleButton onClick={() => (ref as React.RefObject<HTMLDialogElement>).current?.close()}>
-          {buttonText || "Acept"}
-        </SimpleButton>
-      </Modal>
-    );
-  }
-);
+const ModalSimple = ({ text, buttonText, ref, ...rest }: Props) => {
+  const internalRef = useRef<HTMLDialogElement>(null);
+
+  useEffect(() => {
+    if (ref && internalRef.current) {
+      if (typeof ref === "function") {
+        ref(internalRef.current);
+      } else if ("current" in ref) {
+        ref.current = internalRef.current;
+      }
+    }
+  }, [ref]);
+
+  return (
+    <Modal ref={internalRef} {...rest}>
+      <p>{text}</p>
+      <SimpleButton onClick={() => internalRef.current?.close()}>
+        {buttonText || "Accept"}
+      </SimpleButton>
+    </Modal>
+  );
+};
 
 const Modal = styled.dialog`
   padding: 1rem;
